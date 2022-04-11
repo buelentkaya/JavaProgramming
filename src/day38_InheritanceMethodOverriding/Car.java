10952b8775730ebc2bbf58c38f496fc801700c69
Bulent Kaya <97361091+buelentkaya@users.noreply.github.com>
2022-04-11 12:16:31 +0200

    Inheritance MethodOverriding

---
+++ src/day38_InheritanceMethodOverriding/Car.java
@@ -0,0 +1,10 @@
+package day38_InheritanceMethodOverriding;
+
+public class Car {
+    public String brand, model;
+    public int year;
+    public double price;
+    public String color;
+    public int miles;
+
+}
