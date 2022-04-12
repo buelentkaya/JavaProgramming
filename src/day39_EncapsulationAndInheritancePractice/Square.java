57b3ec6a39109b893aaa3d28a74e176a3301affe
Bulent Kaya <97361091+buelentkaya@users.noreply.github.com>
2022-04-12 13:58:18 +0200

    Square has been created

--- src/day39_EncapsulationAndInheritancePractice/Square.java
+++ src/day39_EncapsulationAndInheritancePractice/Square.java
@@ -1,6 +1,26 @@
 package day39_EncapsulationAndInheritancePractice;
 
-public class Square{
+public class Square extends Shape{
+private double side;
+
+    public Square( double side) {
+        super("Square");
+        setSide(side);
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        if(side<=0){
+            System.err.println("Invalid Side ="+side);
+            System.exit(1);
+        }
+        this.side = side;
+    }
+
+
 
 
 }
