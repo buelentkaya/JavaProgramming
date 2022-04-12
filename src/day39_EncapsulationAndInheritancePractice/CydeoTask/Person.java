4c9fc8d0294df2b76342fc4f69c93c64fefad2a8
Bulent Kaya <97361091+buelentkaya@users.noreply.github.com>
2022-04-12 18:40:02 +0200

    person super class done

--- src/day39_EncapsulationAndInheritancePractice/CydeoTask/Person.java
+++ src/day39_EncapsulationAndInheritancePractice/CydeoTask/Person.java
@@ -1,4 +1,71 @@
 package day39_EncapsulationAndInheritancePractice.CydeoTask;
 
 public class Person {
+    private String name;
+    private int age;
+    private char gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null) {
+            System.err.println("Name cannot be null");
+            System.out.println(1);// 1 means that something went rong
+            // 0 verirsen programi sonlamdirir ama 1 verirsen bi seyler ters gitti
+        }
+        if(name.isEmpty()||name.isBlank()){
+            System.err.println("Invalid Name");
+            System.exit(1);
+        }
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age<=0){
+            System.err.println("Invalid Input for the age:"+age);
+            System.exit(1);
+        }
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+
+        this.gender = gender;
+    }
+
+    public Person(String name, int age, char gender) {
+        setName(name);
+        setAge(age);
+        setGender(gender);
+    }
+
+    public void eat(){
+        System.out.println(name+ " is eating.");
+    }
+    public void drink(){
+        System.out.println(name + " is drinking");
+    }
+    public void sleeping(){
+        System.out.println(name+" is sleeping");
+    }
+
+
+
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
 }
